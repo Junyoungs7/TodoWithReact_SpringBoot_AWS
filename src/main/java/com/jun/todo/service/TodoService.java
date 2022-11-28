30d0f26812c86354a1071feef9eee792375e9bed
junyoungs7 <junyoungs7@naver.com>
2022-11-28 21:21:45 +0900

    Initial commit

---
+++ src/main/java/com/jun/todo/service/TodoService.java
@@ -0,0 +1,11 @@
+package com.jun.todo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+    
+    public String testService(){
+        return "Test Service";
+    }
+}
