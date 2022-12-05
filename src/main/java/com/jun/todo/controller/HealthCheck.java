0a0c7336bc1ce80241e295f494754011bcd66f5e
junyoungs7 <junyoungs7@naver.com>
2022-12-05 14:21:09 +0900

    22-12-05 : 배포

---
+++ src/main/java/com/jun/todo/controller/HealthCheck.java
@@ -0,0 +1,13 @@
+package com.jun.todo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/")
+    public String healthCheck(){
+        return "The service is up and running...";
+    }
+}
